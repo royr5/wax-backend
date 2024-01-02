5a9a2a9af657cf891b25bb040b9830ac7482957c
roshan <roshanroy.rr@outlook.com>
2024-01-02 14:06:44 +0000

    install tsc

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const api_router_1 = __importDefault(require("./api/routes/api.router"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use("/", api_router_1.default);
+exports.default = app;
