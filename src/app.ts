4497d7a07e1875a86e79a276666622e1061206b0
roshan <roshanroy.rr@outlook.com>
2024-01-04 16:56:14 +0000

    add db-setup and cors

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,16 @@ import express from "express";
 import apiRouter from "./api/routes/api.router";
 import reviewRouter from "./api/routes/review.router";
 import loginRouter from "./api/routes/login.router";
-import musicRouter from './api/routes/music.router'
+import musicRouter from "./api/routes/music.router";
+import cors from "cors";
 
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
-app.use('/api', apiRouter)
-app.use('/api/music', musicRouter)
-
+app.use("/api", apiRouter);
+app.use("/api/music", musicRouter);
 
 app.use("/api/reviews", reviewRouter);
 
@@ -20,4 +21,4 @@ app.use("/api/login", loginRouter);
 //   res.status(404).send({ msg: 'incorrect path - path not found' })
 // })
 
-export default app
+export default app;
