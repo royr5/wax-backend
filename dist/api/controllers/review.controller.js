89ac3a5e7f1deda61e3ef694629d72dfb334dcb7
ari-abendstern <ari.abendstern@gmx.com>
2024-01-05 10:18:20 +0000

    remove dummy data

--- dist/api/controllers/review.controller.js
+++ dist/api/controllers/review.controller.js
@@ -1,50 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllReviews = exports.getReviewsById = void 0;
+const review_models_1 = require("../models/review.models");
 const getReviewsById = async (req, res, next) => {
     const { music_id } = req.params;
     try {
-        //  TODO
-        //? const reviews = await selectReviews(music_id);
-        // res.status(200).send({ reviews });
-        res.status(200).send({
-            reviews: [
-                {
-                    music_id: 1,
-                    user_id: 2,
-                    score: 2,
-                    title: "string",
-                    body: "string",
-                    created_at: "string",
-                },
-            ],
-        });
+        const reviews = await (0, review_models_1.selectReviews)(music_id);
+        res.status(200).send({ reviews });
     }
     catch (err) {
         next(err);
     }
 };
 exports.getReviewsById = getReviewsById;
-const getAllReviews = async (req, res, next) => {
+const getAllReviews = async (_req, res, next) => {
     try {
-        //  TODO
-        //? const reviews = await selectReviews();
-        // res.status(200).send({ reviews });
-        res.status(200).send({
-            reviews: [
-                {
-                    music_id: 1,
-                    user_id: 2,
-                    score: 2,
-                    title: "string",
-                    body: "string",
-                    created_at: "string",
-                },
-            ],
-        });
+        const reviews = await (0, review_models_1.selectReviews)();
+        res.status(200).send({ reviews });
     }
     catch (err) {
-        next(err);
+        console.log(err);
     }
 };
 exports.getAllReviews = getAllReviews;
