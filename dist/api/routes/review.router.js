e877e4f5e09380c9b2fe97b98f25596fe096fff5
ari-abendstern <ari.abendstern@gmx.com>
2024-01-05 16:58:38 +0000

    add routing for POST review by ID

--- dist/api/routes/review.router.js
+++ dist/api/routes/review.router.js
@@ -4,5 +4,5 @@ const express_1 = require("express");
 const review_controller_1 = require("../controllers/review.controller");
 const reviewRouter = (0, express_1.Router)();
 reviewRouter.route("/").get(review_controller_1.getAllReviews);
-reviewRouter.route("/:music_id").get(review_controller_1.getReviewsById);
+reviewRouter.route("/:music_id").get(review_controller_1.getReviewsById).post(review_controller_1.postReviewById);
 exports.default = reviewRouter;
--- src/api/routes/review.router.ts
+++ src/api/routes/review.router.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import {
   getAllReviews,
   getReviewsById,
+  postReviewById,
 } from "../controllers/review.controller";
 
 const reviewRouter = Router();
 
 reviewRouter.route("/").get(getAllReviews);
-reviewRouter.route("/:music_id").get(getReviewsById);
+reviewRouter.route("/:music_id").get(getReviewsById).post(postReviewById)
 
 export default reviewRouter;
