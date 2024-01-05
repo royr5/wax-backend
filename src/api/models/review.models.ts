a44166a5d56d87c2ca7f10ff81e02a9cc02ff17c
ari-abendstern <ari.abendstern@gmx.com>
2024-01-05 16:59:41 +0000

    add model for POST review by ID

--- src/api/models/review.models.ts
+++ src/api/models/review.models.ts
@@ -16,3 +16,28 @@ export const selectReviews = async (id?: string): Promise<Review[]> => {
   const { rows } = await db.query(formattedQuery);
   return rows as Review[];
 };
+
+export const insertReview = async (music_id: string, screen_name: string, rating: number, review_title?: string, review_body?: string): Promise<Review[]> => {
+
+  const {rows: [review]} = await db.query(
+    `INSERT INTO reviews (
+      music_id,
+      screen_name,
+      rating,
+      review_title,
+      review_body,
+      created_at
+      )
+    VALUES (
+      $1,
+      $2,
+      $3,
+      $4,
+      $5,
+      NOW()
+    )
+    RETURNING *;`,
+    [music_id, screen_name, rating, review_title, review_body]
+  )
+  return review
+}
