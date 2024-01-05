dabd20e44c5dc538007f4a1f270fcc696305e2c7
ari-abendstern <ari.abendstern@gmx.com>
2024-01-05 09:50:26 +0000

    remove dummy review objs

--- src/api/controllers/review.controller.ts
+++ src/api/controllers/review.controller.ts
@@ -9,50 +9,22 @@ export const getReviewsById = async (
   const { music_id } = req.params;
 
   try {
-    //  TODO
-    //? const reviews = await selectReviews(music_id);
-    // res.status(200).send({ reviews });
-
-    res.status(200).send({
-      reviews: [
-        {
-          music_id: 1,
-          user_id: 2,
-          score: 2,
-          title: "string",
-          body: "string",
-          created_at: "string",
-        },
-      ],
-    });
+    const reviews = await selectReviews(music_id);
+    res.status(200).send({ reviews });
   } catch (err) {
     next(err);
   }
 };
 
 export const getAllReviews = async (
-  req: Request,
+  _req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   try {
-    //  TODO
-    //? const reviews = await selectReviews();
-    // res.status(200).send({ reviews });
-
-    res.status(200).send({
-      reviews: [
-        {
-          music_id: 1,
-          user_id: 2,
-          score: 2,
-          title: "string",
-          body: "string",
-          created_at: "string",
-        },
-      ],
-    });
+    const reviews = await selectReviews();
+    res.status(200).send({ reviews });
   } catch (err) {
-    next(err);
+    console.log(err);
   }
 };
