0b2293f708e34aec57a5dd78e13dd8efb5f9afa3
caro <karobarari@gmail.com>
2024-01-09 10:37:17 +0000

    making spotify access files

---
+++ src/api/controllers/search.controller.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Response, Request,} from "express";
+import { spotifyAccess } from "./login.controller";
+
+
+export const getSearchedMusic =()=>{
+}
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import loginRouter from "./api/routes/login.router";
 import musicRouter from './api/routes/music.router'
 import {handle404, handleCustomError, handlePsql23502, handlePsql23503} from './errors'
 import serverless from "serverless-http";
+import searchRouter from "./api/routes/search.router";
 
 
 const app = express();
@@ -17,6 +18,8 @@ app.use("/api/music", musicRouter);
 app.use("/api/reviews", reviewRouter);
 
 app.use("/api/login", loginRouter);
+
+app.use("/api/search",searchRouter)
 
 app.use(handleCustomError);
 
