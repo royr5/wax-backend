09ef4c526b9fd138655d2bb6cf3ec9481c463319
munaciella <franes88@gmail.com>
2024-01-09 12:18:53 +0000

    added a script for users

--- dist/db/mongodb/seed/seed.js
+++ dist/db/mongodb/seed/seed.js
@@ -7,11 +7,14 @@ const connection_1 = __importDefault(require("../connection"));
 const test_data_json_1 = require("../../postgres/data/test-data.json");
 async function addUsers() {
     try {
-        const database = await connection_1.default.db('gatefold_users');
-        const usersCol = database.collection('users');
+        const database = await connection_1.default.db("gatefold_users");
+        const usersCol = database.collection("users");
         const drop = await usersCol.deleteMany({});
         console.log(`Users removed- ${drop}`);
-        const result = await usersCol.insertMany(test_data_json_1.users);
+        const testMongoUsers = test_data_json_1.users.map((user) => {
+            return { username: user.username, password: "test" };
+        });
+        const result = await usersCol.insertMany(testMongoUsers);
         console.log(`Inserted ids: ${result.insertedIds}`);
     }
     finally {
--- src/db/mongodb/seed/seed.ts
+++ src/db/mongodb/seed/seed.ts
@@ -1,28 +1,23 @@
-import client from "../connection"
-import { MongoUsers } from "../../../types/api"
-import {users} from "../../postgres/data/test-data.json"
+import client from "../connection";
+import { MongoUsers } from "../../../types/api";
+import { users } from "../../postgres/data/test-data.json";
 
 async function addUsers() {
-    try{
+  try {
+    const database = await client.db("gatefold_users");
+    const usersCol = database.collection<MongoUsers>("users");
+    const drop = await usersCol.deleteMany({});
+    console.log(`Users removed- ${drop}`);
 
-        const database = await client.db('gatefold_users')
-        const usersCol = database.collection<MongoUsers>('users')
-        const drop = await usersCol.deleteMany({})
-        console.log(`Users removed- ${drop}`);
+    const testMongoUsers = users.map((user) => {
+      return { username: user.username, password: "test" };
+    });
 
-        const testMongoUsers = users.map(user => {
-            return {username: user.username , password: 'test'}
-        })
-        
-        console.log(testMongoUsers)
-        
+    const result = await usersCol.insertMany(testMongoUsers);
 
-        const result = await usersCol.insertMany(testMongoUsers)
-        
-        console.log(`Inserted ids: ${result.insertedIds}`);
-        
-    } finally {
-        await client.close()
-    }
+    console.log(`Inserted ids: ${result.insertedIds}`);
+  } finally {
+    await client.close();
+  }
 }
-addUsers().catch(console.dir)
+addUsers().catch(console.dir);
